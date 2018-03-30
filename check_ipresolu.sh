76564b20fc9240d5dee496af7a344bcd281779c0
Changhao <wu_chang_hao@qq.com>
2018-03-30 14:55:09 +0800

    201803301455

--- check_ipresolu.sh
+++ check_ipresolu.sh
@@ -1,14 +1,21 @@
-i=1
-while true
-do
-        ping linuxch.cn -c 1 > /dev/null 2>&1
-        if [ $? -ne 0 ];then
-                echo "Unable to obtain DNS!!!!$i"       
-                i=`expr $i + 1`
-                sleep 10
-                continue
-        else
-                echo " success !!!"
-                exit
-        fi
-done
+function ip_resolu(){
+	i=1
+	while true
+	do
+			ping linuxch.cn -c 1 > /dev/null 2>&1
+			if [ $? -ne 0 ];then
+					echo "Unable to obtain DNS!!!!$i"       
+					i=`expr $i + 1`
+					sleep 10
+					continue
+			else
+					echo " success !!!"
+					exit
+			fi
+	done
+}
+
+function main(){
+	ip_resolu
+}
+main
