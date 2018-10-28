d6fdcc685ec22d3f0ac9ef7087eb9c0cfa19d2ce
changhao <changhao.ubuntu.com>
2018-10-28 14:28:15 +0800

    push jichu c code

---
+++ C/course/day2/Untitled1.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+int main(void)
+{
+
+    /*
+    int a = 24000, b =12345;
+    int c;
+    c = a + b;
+    printf("a + b = %d \n", c);
+
+    putchar('A');
+    char c1;
+    //c1 = getchar();
+    printf("%d", c1);
+
+    printf("-----------------------");
+
+    int a1 = 12;
+    float b1 = 3.1415;
+    char cc = 'A';
+    printf("%d\n", a1);
+    printf("o%o\n", a1);
+    printf("0x%x\n", a1);
+    //printf("%3.2f\n", a1);
+    printf("%c\n", cc);
+    //getchar();
+
+    printf("----------------------\n")
+    /*使用可变宽度输出字段*/
+
+    /*
+    unsigned width, precision;
+    int number = 256;
+    double wei
+
+
+
+    printf("changhao, hello!!!\n");
+
+    float a;
+    //scanf("%10f", &a);  //正确
+    scanf("%10.2f", &a);
+
+    printf("%f", a);
+
+    int num;
+    printf("Please enter three number:\n");
+    scanf("%*d %*d %d", &num);
+    printf("The last number is %d\n", num);
+
+
+    int a = 99;
+    char b = 'w';
+    double c = 3.1415926;
+    char str[100] = "www.dotcpp.com";
+
+    printf("%d\n", a);  //print整数
+    printf("%c\n", b);  //print字符
+    printf("%f\n", c);  //print浮点数
+    printf("%s\n", str);    //print 字符串
+
+    //1.多进制转换功能
+    int a = 99;
+    printf("%d %o %x\n", a, a, a);
+
+    //2.控制输出占位宽、左右对齐
+    int a = 1;
+    while(a < 100000)
+    {
+        printf("%d\n", a);
+        a = a*10;
+    }
+
+
+    int a = 1;
+    while(a<100000)
+    {
+        printf("%10d\n", a);
+        a = a * 10;
+    }
+
+    int a = 1;
+    while(a<100000)
+    {
+        printf("%-10d\n", a);
+        a = a * 10;
+    }
+
+
+
+    int a = 1;
+    while(a<100000)
+    {
+        printf("%-10d\n", a);
+        a = a * 10;
+    }
+
+
+
+    // 3.小数位数控制
+    float a = 3.14;
+    printf("%.2f\n", a);
+
+    */
+
+    // 4. *号的用法
+    float a = 3.14
+    int b = 5;
+    printf("%*.2f\n",a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
