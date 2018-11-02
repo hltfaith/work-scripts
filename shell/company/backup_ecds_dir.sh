babfe4d309a356ae0aa1713297ab74f0b32f3e91
Changhao <wu_chang_hao@qq.com>
2018-11-02 17:17:51 +0800

    Create backup_ecds_dir.sh

---
+++ shell/company/backup_ecds_dir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## 留存前五天数据文件，其余文件备份至FTP(172.16.6.105) /dianpiao/ECDS
+
+cd /weblogic/user_projects/domains/base_domain/workflow/ECDS
+getdir=`ls -l /weblogic/user_projects/domains/base_domain/workflow/ECDS | awk '{print $NF}' | grep 20 | grep -v $(date +%Y-%m-%d --date='4 days ago') | grep -v $(date +%Y-%m-%d --date='3 days ago') | grep -v $(date +%Y-%m-%d --date='2 days ago') | grep -v $(date +%Y-%m-%d --date='1 days ago') | grep -v $(date +%Y-%m-%d --date='0 days ago')`
+
+if [ "$getdir" != "" ]
+then
+        tar zcvf $getdir.tar.gz $getdir/*
+
+ftp -n <<!
+open 172.16.6.105
+user log log 
+cd dianpiao/ECDS
+binary
+prompt
+mput *.tar.gz
+bye
+!
+
+        rm -rf $getdir.tar.gz
+        rm -rf $getdir
+fi
