0530e004af5229b00b138fe6346934b228b99ad3
Changhao <wu_chang_hao@qq.com>
2018-11-25 10:58:12 +0800

    Create case01.c

---
+++ C/usage/switch/case01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int value = 1;
+
+	switch (value)
+	{
+	case 1:
+		printf("one\n");
+		break;
+	case 2:
+		printf("two\n");
+		break;
+	case 3:
+		printf("three\n");
+		break;
+	}
+	system("pause");
+}
