f9a979a27ab1e5f8146a84dbcbba3b600ab09cda
Changhao <wu_chang_hao@qq.com>
2018-11-25 10:21:00 +0800

    Create case01.c

---
+++ C/usage/if/case01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+
+	int flag = 20;
+	if (flag <= 90)
+	{
+		printf("changhao ok!!\n");
+	}
+	else if (flag = 60)
+	{
+		printf("changhao yiban ok!!\n");
+	}
+	else
+	{
+		printf("changhao error!!!\n");
+	}
+	system("pause");
+
+}
