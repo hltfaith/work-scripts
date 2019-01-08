32215e37a112ab5117b145cc55c93710956ba44e
Changhao <wu_chang_hao@qq.com>
2019-01-08 19:55:39 +0800

    Update 基础命令.sh

--- linux/commands/develop/git/use/基础命令.sh
+++ linux/commands/develop/git/use/基础命令.sh
@@ -5,3 +5,7 @@ git branch -r #查看所有远程分支
 git branch #查看所有本地分支
 git branch -d -r origin/branchA #删除远程分支
 
+可以使用git merge命令或者git rebase命令，在本地分支上合并远程分支。
+git merge origin/master
+# 或者
+git rebase origin/master
