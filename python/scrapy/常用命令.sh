e4203f833ab5321ac05ac6aa7e2c856d39178b11
Changhao <wu_chang_hao@qq.com>
2019-04-14 00:35:17 +0800

    Update 常用命令.sh

--- python/scrapy/常用命令.sh
+++ python/scrapy/常用命令.sh
@@ -5,3 +5,29 @@ scrapy startproject test1
 # 进入到项目目录scrapy genspider 爬虫名字 爬虫的域名
 scrapy genspider ncore12306 www.12306.cn
 
+# 关于命令详细使用
+echo "
+命令的使用范围
+
+这里的命令分为全局的命令和项目的命令，全局的命令表示可以在任何地方使用，而项目的命令只能在项目目录下使用
+
+全局的命令有：
+startproject
+genspider
+settings
+runspider
+shell
+fetch
+view
+version
+
+项目命令有：
+crawl
+check
+list
+edit
+parse
+bench
+"
+
+
