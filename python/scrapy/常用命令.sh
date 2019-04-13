63d3b30488044dd680f6391a6c159e434bce0e1a
Changhao <wu_chang_hao@qq.com>
2019-04-14 00:39:55 +0800

    Update 常用命令.sh

--- python/scrapy/常用命令.sh
+++ python/scrapy/常用命令.sh
@@ -30,4 +30,45 @@ parse
 bench
 "
 
+# genspider
+# 用于生成爬虫，这里scrapy提供给我们不同的几种模板生成spider,默认用的是basic,我们可以通过命令查看所有的模板
+scrapy genspider -l
+
+
+# check 用于检查代码是否有错误
+scrapy check
+
+# list 列出所有可用的爬虫
+scrapy list
+
+# fetch 该命令会通过scrapy downloader 讲网页的源代码下载下来并显示出来
+scrapy fetch url地址 
+canshu="
+这里有一些参数：
+--nolog 不打印日志
+--headers 打印响应头信息
+--no-redirect 不做跳转"
+
+# view 该命令会讲网页document内容下载下来，并且在浏览器显示出来
+scrapy view url地址
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
