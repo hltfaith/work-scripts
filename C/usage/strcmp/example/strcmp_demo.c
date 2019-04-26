5245bc4ef87945d1bf8c9e1018753ac380ab9455
changhao <changhao.ubuntu.com>
2019-04-26 23:09:40 +0800

    add strcmp()

---
+++ C/usage/strcmp/example/strcmp_demo.c
@@ -0,0 +1,16 @@
+//
+// Created by changhao on 19-4-26.
+//
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char a[] = "aBcDeF";
+    char b[] = "AbCdEf";
+    char c[] = "aacdef";
+    char d[] = "aBcDeF";
+    printf("a VS b: %d\n", strcmp(a, b));
+    printf("a VS c: %d\n", strcmp(a, c));
+    printf("a VS d: %d\n", strcmp(a, d));
+
+    return 0;
+}
