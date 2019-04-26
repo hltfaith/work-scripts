4e0d11bd48eb4ce005e863937b97a83f60f45a9a
Changhao <wu_chang_hao@qq.com>
2019-04-26 22:00:20 +0800

    Create 判断字符串长度.c

---
+++ C/usage/arrayName/example/判断字符串长度.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+    char str[] = "http://c.biancheng.net/c/";
+    long len = strlen(str);
+
+    printf("The lenth of the string is %ld.\n", len);
+
+    return 0;
+
+}
