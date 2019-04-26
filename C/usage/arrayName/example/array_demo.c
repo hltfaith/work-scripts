b46b158480d9b0eef34b70c411c8d6b71c8f65e0
Changhao <wu_chang_hao@qq.com>
2019-04-26 21:03:49 +0800

    Update array_demo.c

--- C/usage/arrayName/example/array_demo.c
+++ C/usage/arrayName/example/array_demo.c
@@ -27,7 +27,25 @@ int main(void) {
     {
         printf("%d", nums[i]);
     }
+    
+    /*
 
+
+    int nums[10];
+    int i;
+
+    //从控制台读取用户输入
+    for(i=0; i<10; i++){
+        scanf("%d", &nums[i]);  //注意取地址符 &，不要遗忘哦
+    }
+
+    //依次输出数组元素
+    for(i=0; i<10; i++){
+        printf("%d ", nums[i]);
+    }
+    
+    */
+    
     return 0;
 
 
