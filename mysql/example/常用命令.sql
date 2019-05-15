dcfdb9d7cf418f56a1bb0910d207d98a6cad802a
Changhao <wu_chang_hao@qq.com>
2019-05-16 02:52:45 +0800

    Create 常用命令.sql

---
+++ mysql/example/常用命令.sql
@@ -0,0 +1,12 @@
+
+show databases;
+show tables;
+use ticketdb;
+select database();
+show variables like '%char%';
+desc userinfo;
+show variables like '%general_log%';
+SHOW TABLE STATUS;
+select @@IDENTITY;
+show create table followticket;
+
